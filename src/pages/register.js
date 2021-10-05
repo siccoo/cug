77192aee4415edbe2f598ee5965d77c50986e1ed
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-10-05 15:59:25 +0100

    stating the success & failure components

--- src/pages/register.js
+++ src/pages/register.js
@@ -4,6 +4,8 @@ import Footer from "../components/footer";
 import Zones from "./zones/Zones";
 import policeCugApi from "../utils/policeCugApi";
 import { useForm, Form } from "../hooks/useForm";
+import Success from "./success/Success";
+import Failure from "./failure/Failure";
 
 const initialValues = {
   firstName: "",
@@ -19,6 +21,9 @@ export default function Register() {
   const [nigeriaZones, setNigeriaZones] = useState("");
   const [nigeriaStates, setNigeriaStates] = useState("");
   const { values, handleChange, errors, setErrors } = useForm(initialValues);
+  const [views, setViews] = useState(true);
+  const [viewsSuccess, setViewsSuccess] = useState(false);
+  const [viewsFailure, setViewsFailure] = useState(false);
 
   const zoneList = Object.keys(Zones).map((key) => ({
     name: key,
@@ -90,6 +95,9 @@ export default function Register() {
     // }
   };
 
+  const successComponent = viewsSuccess ? <Success /> : null;
+  const failureComponent = viewsFailure ? <Failure /> : null;
+
   return (
     <div>
       <Header />
@@ -100,139 +108,145 @@ export default function Register() {
           </div>
         </div>
       </div>
-      <div className="itex-form-section wf-section">
-        <div className="itex-form">
-          <div className="itex-form-header">
-            Please enter your details below
-            <p className="itex-form-subheader">
-              Disclaimer: Any information inputted is confidential and not to be
-              re-shared with any party.
-            </p>
-          </div>
-
-          <div className="itex-form-box">
-            <Form onSubmit={handleSubmit}>
-              <div className="individual-form">
-                <div>
-                  <label className="label">
-                    First Name <span>*</span>
-                  </label>
-                  <input
-                    className="textfield-control"
-                    id="outlined-basic"
-                    name="firstName"
-                    type="text"
-                    value={values.firstName}
-                    onChange={handleChange}
-                    error={errors.firstName}
-                  />
-                </div>
-
-                <div>
-                  <label className="label">
-                    Last Name <span>*</span>
-                  </label>
-                  <input
-                    className="textfield-control"
-                    id="outlined-basic"
-                    name="lastName"
-                    type="text"
-                    value={values.lastName}
-                    onChange={handleChange}
-                    error={errors.lastName}
-                  />
-                </div>
-
-                <div>
-                  <label className="label">
-                    Phone Number <span>*</span>
-                  </label>
-                  <input
-                    className="textfield-control"
-                    id="outlined-basic"
-                    name="phoneNumber"
-                    type="tel"
-                    value={values.phoneNumber}
-                    onChange={handleChange}
-                    error={errors.phoneNumber}
-                  />
-                </div>
-                <div>
-                  <label className="label">
-                    Police Staff ID <span>*</span>
-                  </label>
-                  <input
-                    className="textfield-control"
-                    id="outlined-basic"
-                    name="staffId"
-                    type="text"
-                    value={values.staffId}
-                    onChange={handleChange}
-                    error={errors.staffId}
-                  />
-                </div>
-                <div>
-                  <label className="label">
-                    Email address <span>*</span>
-                  </label>
-                  <input
-                    className="textfield-control"
-                    id="outlined-basic"
-                    name="email"
-                    type="email"
-                    value={values.email}
-                    onChange={handleChange}
-                    error={errors.email}
-                  />
-                </div>
-
-                <div>
-                  <label className="label">
-                    Zone <span>*</span>
-                  </label>
-                  <select
-                    className="textfield-control"
-                    id="demo-simple-select"
-                    value={nigeriaZones}
-                    error={errors.nigeriaZones}
-                    onChange={(e) => handleZonesSelect(e)}
-                  >
-                    <option value=""></option>
-                    {zoneList.map((zone, key) => (
-                      <option key={key} value={zone.name}>
-                        {zone.name.substring(0, 4)} {zone.name.slice(4)}
-                      </option>
-                    ))}
-                  </select>
-                </div>
-
-                <div>
-                  <label className="label">
-                    State <span>*</span>
-                  </label>
-                  <select
-                    className="textfield-control"
-                    id="demo-simple-select"
-                    value={nigeriaStates}
-                    error={errors.nigeriaStates}
-                    onChange={(e) => handleStatesSelect(e)}
-                  >
-                    <option value=""></option>
-                    {areas.map((state, key) => (
-                      <option key={key} value={state}>
-                        {state}
-                      </option>
-                    ))}
-                  </select>
-                </div>
+      {views === true ? (
+        <>
+          <div className="itex-form-section wf-section">
+            <div className="itex-form">
+              <div className="itex-form-header">
+                Please enter your details below
+                <p className="itex-form-subheader">
+                  Disclaimer: Any information inputted is confidential and not
+                  to be re-shared with any party.
+                </p>
               </div>
-              <button type="submit" className="itex-form-submit-btn">
-                Submit
-              </button>
-            </Form>
+
+              <div className="itex-form-box">
+                <Form onSubmit={handleSubmit}>
+                  <div className="individual-form">
+                    <div>
+                      <label className="label">
+                        First Name <span>*</span>
+                      </label>
+                      <input
+                        className="textfield-control"
+                        id="outlined-basic"
+                        name="firstName"
+                        type="text"
+                        value={values.firstName}
+                        onChange={handleChange}
+                        error={errors.firstName}
+                      />
+                    </div>
+
+                    <div>
+                      <label className="label">
+                        Last Name <span>*</span>
+                      </label>
+                      <input
+                        className="textfield-control"
+                        id="outlined-basic"
+                        name="lastName"
+                        type="text"
+                        value={values.lastName}
+                        onChange={handleChange}
+                        error={errors.lastName}
+                      />
+                    </div>
+
+                    <div>
+                      <label className="label">
+                        Phone Number <span>*</span>
+                      </label>
+                      <input
+                        className="textfield-control"
+                        id="outlined-basic"
+                        name="phoneNumber"
+                        type="tel"
+                        value={values.phoneNumber}
+                        onChange={handleChange}
+                        error={errors.phoneNumber}
+                      />
+                    </div>
+                    <div>
+                      <label className="label">
+                        Police Staff ID <span>*</span>
+                      </label>
+                      <input
+                        className="textfield-control"
+                        id="outlined-basic"
+                        name="staffId"
+                        type="text"
+                        value={values.staffId}
+                        onChange={handleChange}
+                        error={errors.staffId}
+                      />
+                    </div>
+                    <div>
+                      <label className="label">
+                        Email address <span>*</span>
+                      </label>
+                      <input
+                        className="textfield-control"
+                        id="outlined-basic"
+                        name="email"
+                        type="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        error={errors.email}
+                      />
+                    </div>
+
+                    <div>
+                      <label className="label">
+                        Zone <span>*</span>
+                      </label>
+                      <select
+                        className="textfield-control"
+                        id="demo-simple-select"
+                        value={nigeriaZones}
+                        error={errors.nigeriaZones}
+                        onChange={(e) => handleZonesSelect(e)}
+                      >
+                        <option value="">Force Headquarters</option>
+                        {zoneList.map((zone, key) => (
+                          <option key={key} value={zone.name}>
+                            {zone.name.substring(0, 4)} {zone.name.slice(4)}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+
+                    <div>
+                      <label className="label">
+                        State <span>*</span>
+                      </label>
+                      <select
+                        className="textfield-control"
+                        id="demo-simple-select"
+                        value={nigeriaStates}
+                        error={errors.nigeriaStates}
+                        onChange={(e) => handleStatesSelect(e)}
+                      >
+                        <option value=""></option>
+                        {areas.map((state, key) => (
+                          <option key={key} value={state}>
+                            {state}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+                  </div>
+                  <button type="submit" className="itex-form-submit-btn">
+                    Submit
+                  </button>
+                </Form>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : null}
+      {successComponent}
+      {failureComponent}
       <Footer />
     </div>
   );
